238076b535ee0d0195988393c5e36aaa82484e50
st4lk <myhappydo@gmail.com>
2013-11-18 15:59:00 +0400

    postman: write message body required

--- stormcrew/postman_custom/forms.py
+++ stormcrew/postman_custom/forms.py
@@ -7,6 +7,7 @@ class WriteFormHideRecipients(WriteForm):
     def __init__(self, *args, **kwargs):
         super(WriteFormHideRecipients, self).__init__(*args, **kwargs)
         self.fields['recipients'].widget = forms.HiddenInput()
+        self.fields['body'].required = True
 
     class Meta(WriteForm.Meta):
         pass
