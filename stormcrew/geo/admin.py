44e9d92ed59d52f23a4db9f4223814533a2ef3ff
st4lk <myhappydo@gmail.com>
2013-11-20 18:37:19 +0400

    import IATA

--- stormcrew/geo/admin.py
+++ stormcrew/geo/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Country, City
+from .models import Country, City, AirportIATA, UploadIATA
 
 
 class CountryAdmin(admin.ModelAdmin):
@@ -12,5 +12,17 @@ class CityAdmin(admin.ModelAdmin):
     list_display = 'name', "country"
 
 
+class AirportIATAAdmin(admin.ModelAdmin):
+    list_display = 'iata', 'name_en', 'name_ru', 'parent_name_en'
+    search_fields = 'name_en', 'name_ru', 'parent_name_en'
+
+
+class UploadIATAAdmin(admin.ModelAdmin):
+    list_display = 'csv_file', 'result'
+    readonly_fields = 'result', 'error_details'
+
+
 admin.site.register(Country, CountryAdmin)
 admin.site.register(City, CityAdmin)
+admin.site.register(AirportIATA, AirportIATAAdmin)
+admin.site.register(UploadIATA, UploadIATAAdmin)
--- stormcrew/geo/models.py
+++ stormcrew/geo/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+import traceback
+import csv
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from model_utils import Choices
 from .managers import CountryManager, CityManager
+from django.db.models.signals import pre_save, pre_delete
 
 
 class Country(models.Model):
@@ -34,3 +39,78 @@ class City(models.Model):
 
     def __unicode__(self):
         return u"{0}, {1}".format(self.country, self.name)
+
+
+class AirportIATA(models.Model):
+
+    iata = models.CharField(max_length=100, blank=True)
+    type = models.CharField(max_length=100, blank=True)
+    name_ru = models.CharField(max_length=100, blank=True, db_index=True)
+    name_en = models.CharField(max_length=100, blank=True, db_index=True)
+    coordinates = models.CharField(max_length=100, blank=True)
+    timezone = models.CharField(max_length=100, blank=True)
+    parent_name_en = models.CharField(max_length=100, blank=True, db_index=True)
+
+    class Meta:
+        verbose_name = _(u'IATA')
+        verbose_name_plural = _(u'IATA')
+
+    def __unicode__(self):
+        return "{0}, {1} ({2})".format(self.name_en, self.parent_name_en, self.iata)
+
+
+class UploadIATA(models.Model):
+
+    RESULT = Choices(
+        (0, 'success', 'Success'),
+        (1, 'process', 'In process'),
+        (2, 'error', 'Error'))
+
+    csv_file = models.FileField(upload_to='iata',
+        help_text=u"Сохранение нового файла удалить все существующие записи IATA и импортирует новые из этого файла")
+    result = models.PositiveSmallIntegerField(choices=RESULT,
+        default=RESULT.process)
+    error_details = models.TextField(blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _(u'Файл csv IATA')
+        verbose_name_plural = _(u'Файлы csv IATA')
+
+    def __unicode__(self):
+        return self.csv_file.path
+
+    @staticmethod
+    def process_uploaded_csv(sender, instance, *args, **kwargs):
+        if not instance.pk:
+            # process upload only for first time
+            try:
+                result = UploadIATA.RESULT.success
+                err_details = ""
+                AirportIATA.objects.all().delete()
+                reader = csv.reader(instance.csv_file, delimiter=",")
+                # skip header line
+                reader.next()
+                for row in reader:
+                    row_data = {}
+                    row_data['iata'] = row[0]
+                    row_data['type'] = row[1]
+                    row_data['name_ru'] = row[2].decode("utf8")
+                    row_data['name_en'] = row[3]
+                    row_data['coordinates'] = row[4]
+                    row_data['timezone'] = row[5]
+                    row_data['parent_name_en'] = row[6]
+                    AirportIATA(**row_data).save()
+                instance.result = result
+                instance.error_details = err_details
+            except Exception:
+                instance.result = UploadIATA.RESULT.error
+                instance.error_details = traceback.format_exc()
+
+    @staticmethod
+    def process_deleted_csv(sender, instance, *args, **kwargs):
+        if instance.csv_file:
+            instance.csv_file.delete(False)
+
+pre_save.connect(UploadIATA.process_uploaded_csv, sender=UploadIATA)
+pre_delete.connect(UploadIATA.process_deleted_csv, sender=UploadIATA)
