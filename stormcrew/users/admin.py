566f75bda5777840c5e7c79e31db20082f295d89
st4lk <myhappydo@gmail.com>
2013-11-20 23:25:42 +0400

    user admin: add social link

--- stormcrew/users/admin.py
+++ stormcrew/users/admin.py
@@ -9,12 +9,15 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     create_form_class = UserCreationForm
     update_form_class = UserChangeForm
-    list_display = 'full_name', 'email', 'provider'
+    list_display = 'full_name', 'email', 'provider', 'social_link'
 
     def full_name(self, obj):
         return obj.get_full_name()
     full_name.short_description = u"Полное имя"
 
+    def social_link(self, obj):
+        return obj.get_social_link()
+    social_link.short_description = u"Профиль в соц. сети"
 
 admin.site.register(User, UserAdmin)
 
