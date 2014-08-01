76201fd1888e3da79b1b96293202c435f64ce328
gregeatworld <gregeatworld@gmail.com>
2014-08-01 04:21:27 +0400

    triprequest injection

--- divvy/static/js/apps/details/main.js
+++ divvy/static/js/apps/details/main.js
@@ -1,8 +1,45 @@
 require([
     'jquery',
+    'rivets',
+    'dataProvider',
     'fotorama'
-], function ($) {
+], function ($,Rivets,DataProvider) {
 
+    var RequestClass = function (data) {
+        this.id = data.get('tripId') || 0;
+        this.resource_uri = '/api/v1/trip/'+this.id+'/';
+    };
+    RequestClass.prototype = {
+        send: function () {
+            var self = this;
+
+            $.ajax({
+                type: "POST",
+                url: "/api/v1/triprequest/",
+                data: {"trip": self.resource_uri},
+                dataType:"json",
+                contentType:"application/json; charset=utf-8",
+                success: function(data) {
+                    console.log('Success',data);
+                },
+                error: function(data) {
+                    console.log('Error',data);
+                }
+            }).then(function(data) {
+                console.log('Deferred promise',data);
+            });
+        },
+        test: function () {
+            console.log('TEST Rivets');
+        }
+    }
+
+    var dataProvider = new DataProvider( window.DDATA );
+    var scope = new RequestClass( dataProvider );
+
+    Rivets.bind($('body'), { scope: scope });
+
+    // Fotorama is not AMD yet
     $('.fotorama').fotorama({
         // width: 700,
         // maxwidth: '100%',
@@ -14,4 +51,5 @@ require([
         fit: 'cover',
         allowfullscreen: false
     });
+
 });
