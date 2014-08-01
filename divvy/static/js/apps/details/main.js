1fbf9b9cc4b982fd90397050f2936152e7acfd9f
gregeatworld <gregeatworld@gmail.com>
2014-08-01 20:35:11 +0400

    triprequest statuses + comments block

--- divvy/static/js/apps/details/main.js
+++ divvy/static/js/apps/details/main.js
@@ -14,11 +14,68 @@ require([
         }
     };
 
+    // Rivets.binders.checked = {
+    //     publishes: true,
+    //     bind: function(el) {
+    //       return Rivets.Util.bindEvent(el, 'change', this.publish);
+    //     },
+    //     unbind: function(el) {
+    //       return Rivets.Util.unbindEvent(el, 'change', this.publish);
+    //     },
+    //     routine: function(el, value) {
+    //       var _ref1;
+    //       if (el.type === 'radio') {
+    //         return el.checked = ((_ref1 = el.value) != null ? _ref1.toString() : void 0) === (value != null ? value.toString() : void 0);
+    //       } else {
+    //         return el.checked = !!value;
+    //       }
+    //     }
+    //   };
+
+    console.log( Rivets );
+
+    // Rivets.binders.stepshow = {
+    //     // publishes: true,
+    //     bind: function(el) {
+    //       return Rivets._.Util.bindEvent(el, 'change', this.publish);
+    //     },
+    //     unbind: function(el) {
+    //       return Rivets._.Util.unbindEvent(el, 'change', this.publish);
+    //     },
+
+    //     routine: function(el, value) {
+    //         if ( $(el).data('type') == value )
+    //             $(el).show();
+    //         else 
+    //             $(el).hide();
+    //     },
+    //     update: function(models) {
+    //         console.log( 'Op Op',models );
+    //     }
+    // };
+
+    // Rivets.binders.stepshow = function(el, value) {
+    //     if ( $(el).data('type') == value )
+    //         $(el).show(100);
+    //     else 
+    //         $(el).hide(100);
+    // }
+
+    Rivets.binders['stepshow-*'] = function(el, value){
+        // el.style.setProperty(this.args[0], value);
+        console.log('Op',this.args[0] );
+        if ( this.args[0] == value )
+            $(el).show();
+        else 
+            $(el).hide();
+    };
+
     var RequestClass = function (data) {
         this.id = data.get('tripId') || 0;
         this.resource_uri = "/api/v1/trip/"+this.id+"/";
         this.requestSend = true;
         this.requestCons = false;
+        this.requestStep = 'send';
         this.____ = RequestClass;
     };
     RequestClass.prototype = {
@@ -32,8 +89,7 @@ require([
                 contentType: "application/json; charset=utf-8",
                 success: function(data) {
                     console.log('Success',data);
-                    self.requestSend = false;
-                    self.requestCons = true;
+                    self.requestStep = 'cons';
                 },
                 error: function(data) {
                     console.log('Error',data);
@@ -42,8 +98,13 @@ require([
                 console.log('Deferred promise',data);
             });
         },
-        test: function () {
-            console.log('TEST Rivets');
+        goSend: function (Event, TargetEle) {
+            var self = this;
+            console.log('goSend');
+            self.requestStep = 'send-approve';
+        },
+        currentStep: function (Event, TargetEle) {
+            console.log(Event, TargetEle);
         }
     };
 
